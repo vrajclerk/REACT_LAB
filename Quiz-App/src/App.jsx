f42cfc1f1d498165ffbde5a555680edd58ddc0c7
VRAJ_CLERK <vrajclerk04@gmail.com>
2024-02-07 00:57:01 +0530

    date-time

---
+++ Quiz-App/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Form from './Components/Form'
+
+function App() {
+  
+  return (
+    <div>
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ Quiz-App/src/Components/Form.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+// function getInTwoDigits(value) {
+//   if (value < 10) {
+//     value = "0" + value;
+//   }
+//   return value;
+// }
+
+function getCurrentTime(){
+  const today = new Date();
+  const h = today.getHours();
+  //const m = today.getMinutes();
+ // const s = today.getSeconds();
+
+  return `${h}`
+  //return `${h}:${getInTwoDigits(m)}:${getInTwoDigits(s)}`
+}
+
+export default function Form() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+  console.log(getCurrentTime(),":");
+  console.log("\tanswer = ", answer);
+  console.log("\tstatus = ", status);
+  console.log("\terror = ", error);
+
+  if (status === "success") {
+    return <h1>That is right!</h1>;
+  }
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Capital city quiz</h2>
+      <p>
+        Which city is the capital of Gujarat?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === "submitting"}>
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error.message}</p>}
+      </form>
+    </>
+  );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "gandhinagar";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
